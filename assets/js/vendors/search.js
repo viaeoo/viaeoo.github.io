341db8f552bc6b4033c6514afe3debae47915511
viaeoo <viaeoo@gmail.com>
2017-11-26 23:06:23 +0900

    update blod

--- assets/js/vendors/search.js
+++ assets/js/vendors/search.js
@@ -53,7 +53,7 @@
     
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
-    var idx = new lunr.Index;
+    var idx = new lunr.Index();
     
     switch(searchTerm.sort) {
       case 'all':
